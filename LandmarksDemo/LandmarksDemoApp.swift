b20f5fbb029318bcb831ca49cd011641126a0978
Apurva Jalgaonkar <apurva.jalgaonkar@sportzinteractive.net>
2024-06-12 12:16:48 +0530

    Initial Commit

---
+++ LandmarksDemo/LandmarksDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksDemoApp.swift
+//  LandmarksDemo
+//
+//  Created by Apurva Jalgaonkar on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
